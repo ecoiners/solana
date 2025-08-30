8730c269e3fe345f28c7afe5992800c31af1ded3
ecoiners <damuna63@gmail.com>
2025-08-31 06:07:32 +0700

    starts first projects

---
+++ src/components/NetworkSwitcher.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react";
+import dynamic from "next/dynamic";
+import NetworkSwitcher from "./SVG/CreateSVG";
+import { useNetworkConfiguration } from "../contexts/NetworkConfigurationProvider";
+
+const NetworkSwitcher: FC = () => {
+	const {networkConfiguration, setNetworkConfiguration} = useNetworkConfiguration();
+	
+  return (
+    <>
+		  <input type="checkbox" id="checkbox"/>
+			<label className="switch">
+			  <select
+				  value={networkConfiguration}
+				  onChange={(e) => setNetworkConfiguration(e.target.value || "devnet")}
+					className="select max-w-xs border-none pink outline-0"
+				>
+				  <option value="mainnet-beta">main</option>
+					<option value="devnet">devnet</option>
+					<option value="testnet">testnet</option>
+				</select>
+			</label>
+		</>
+  );
+};
+
+export default dynamic(() => Promise.resolve(NetworkSwitcher), {ssr:false});
---
+++ src/utils/explorer.ts
@@ -0,0 +1,21 @@
+import {PublicKey, Transaction} from "@solana/web3.js";
+
+export function getExplorerUrl(
+	endpoint: string,
+	viewTypeOrItemAddress: "inspector" | PublicKey | string,
+	itemType = "address"
+) {
+	const getClusterUrlParam = () => {
+		let cluster = "";
+		
+		if (endpoint === "localnet") {
+			cluster = `custom&customUrl=${encodeURIComponent("http://localhost:3000")}`;
+		} else if (endpoint === "https://api.devnet.solana.com") {
+			cluster = "devnet";
+		}
+		
+		return cluster ? `?cluster=${cluster}` : "";
+	};
+	
+	return `https://explorer.solana.com/${itemType}/${viewTypeOrItemAddress}${getClusterUrlParam()}`;
+};
